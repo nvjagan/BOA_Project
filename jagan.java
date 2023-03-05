5ab5faaa65c8e8be85c07c6f0125df058df4e678
nvjagan <109247078+nvjagan@users.noreply.github.com>
2023-03-05 10:01:45 -0600

    Update jagan.java

--- jagan.java
+++ jagan.java
@@ -1,8 +1,8 @@
 class{
 
 <<<<<<< HEAD
-welcome jaganMohan Reddy
-=======
-welcome jaganMohan Reddy SAU university 999901792
->>>>>>> 7d1a6b71d43caa44a3d198aadf87bd086843a3f5
+
+
+welcome jaganMohan Reddy SAU university 999901792 sakshi
+
 }
